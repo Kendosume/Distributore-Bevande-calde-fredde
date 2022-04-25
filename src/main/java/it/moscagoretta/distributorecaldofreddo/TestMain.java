fb543a0923f68ef1fc04e6ff39df2ea8b9b1b697
Matteo Mosca Goretta <matteomoscagoretta@gmail.com>
2022-04-25 11:09:57 +0200

    fix sout "wasd" pt2

--- src/main/java/it/moscagoretta/distributorecaldofreddo/TestMain.java
+++ src/main/java/it/moscagoretta/distributorecaldofreddo/TestMain.java
@@ -235,7 +235,7 @@ public class TestMain {
 
                 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
             } else if (sceltaa.equals("wasd")) {
-                System.out.println("1234: visualizza i prodotti della categoria che selezionerà nel distributore");
+                System.out.println("1234: Visualizza i prodotti della categoria che selezionerà nel distributore");
                 System.out.println("4321: Exit");
             } else {
                 System.out.println("Provi ascrivere con la minuscola la categoria");
